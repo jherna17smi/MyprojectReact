a3948d11e620f1b28d6dfc38613ba2116884597f
Jose A. Hernandez Soto <anthony17smi@gmail.com>
2026-06-06 02:46:59 -0400

    ci: update workflow and remove act warnings

--- src/components/__tests__/App.integration.test.jsx
+++ src/components/__tests__/App.integration.test.jsx
@@ -1,8 +1,8 @@
 // src/__tests__/App.integration.test.jsx
 import React from "react";
+import { act } from "react";
 import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
-import { act } from "react-dom/test-utils";
 import App from "../../App";
 
 describe("App integration", () => {
--- src/components/__tests__/QuantitySelector.test.jsx
+++ src/components/__tests__/QuantitySelector.test.jsx
@@ -1,8 +1,8 @@
 // src/components/__tests__/QuantitySelector.test.jsx
 import React from "react";
+import { act } from "react";
 import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
-import { act } from "react-dom/test-utils";
 import { default as QuantitySelector } from "../QuantitySelector.js";
 
 describe("QuantitySelector", () => {
